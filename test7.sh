b1925dca264e62795a95371a2ba5f33584054942
ping <843951249@qq.com>
2018-07-11 21:56:58 +0800

    阶乘最最终版

--- test7.sh
+++ test7.sh
@@ -4,22 +4,27 @@
 i=1  #全局变量
 
 fun(){
+    if [ $1 -eq 0 ]
+    then
+        return 1
+    fi
     local j=$1   #定义两个局部变量
     local j_1=$[$j-1]
     if [ $j_1 -eq 1 ]
     then
-        i=2
+        return 2
     else
         fun $j_1
-        i=$[$j*$i]
+        i=$[$j*$?]  #这里$j就是为什么不可以用全局变量
     fi
+    return $i
 }
 
 if [ $# -ne 1 ]
-then
+then   #usage信息
     echo "usage: $0 [n]"
     echo "calculates a number's factorial"
 else
     fun $1
-    echo "$i"
+    echo "$?"   #接收返回值
 fi
