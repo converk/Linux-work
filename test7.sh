9deaf8c426db034ac7440d981f0fa21411e0102d
ping <843951249@qq.com>
2018-07-11 21:35:26 +0800

    阶乘完整版

--- test7.sh
+++ test7.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+#传入一个参数,计算阶乘
 
-read m
-j=$m
-while [ $m -ge 2 ]
-do
-    i=$[$m-1]
-    j=$[$j*$i]
-    let "m--"
-done
-echo "$j"
+i=1  #全局变量
+
+fun(){
+    j=$1   #定义两个局部变量
+    j_1=$[$j-1]
+    if [ $j_1 -eq 1 ]
+    then
+        i=2
+    else
+        fun $j_1
+        i=$[$j*$i]
+    fi
+}
+
+if [ $# -ne 1 ]
+then
+    echo "usage: $0 [n]"
+    echo "calculates a number's factorial"
+else
+    fun $1
+    echo "$i"
+fi
