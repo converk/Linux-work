54a274cfafe57787248f0c86aa827317f80d5854
ping <843951249@qq.com>
2018-07-12 16:43:57 +0800

    work2  readme

--- README.md
+++ README.md
@@ -4,16 +4,20 @@
     * .vscode是vs code自动生成
     * test.sh到test6.sh是学习shell中的一些小实例
     * 笔记是学习shell中自己总结的一些比较容易错的用法和语法
-    * work1.sh是递归阶乘的代码
+    * **work1.sh**是递归阶乘的代码
+    * **work2.sh**是自动解压的代码
 
 ## 二,运行结果
-**work1.sh**
+**阶乘**
 
     hbw@hbw-Vostro-3559:~/shell$ ./work1.sh 6
     720
     hbw@hbw-Vostro-3559:~/shell$ ./work1.sh
     usage: ./work1.sh [n]
     calculates a number's factorial
+**自动解压缩**
+
+    ![Alt text](/home/hbw/图片/2.png)
 
 ## 三,解题思路
 **阶乘**
@@ -25,3 +29,7 @@
         局部变量用来循环调用,当j_1=1时开始返回,
         用i接受最终的值,
         echo "$i"输出
+**自动解压缩**
+
+    根据不同的参数来执行不同的命令,当输入的是正确的路径,就先进入目的路径,之后再根据文件的后缀名来判断
+    按照哪个方式来解压,输入错误的指令,根据情况输出提示信息
--- work2.sh
+++ work2.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-if [ $# -eq 1 -a $1 = "--list" ]
+if [ $# -eq 1 -a $1 = "--list" ]  #当只有一个参数,而且是--list时显示支持的类型
 then
     echo "Supported file types :zip tar tar.gz tar.bz2"
-elif [ $# -eq 2 ]
+elif [ $# -eq 2 -o $# -eq 1 ]
 then
-    cd $2
-    i=${$1##*.}  #提取文件后缀
-    if [ i = gz ]
+    file1=$1
+    if [ $# -eq 2 ]
+    then
+        cd $2
+    else
+        cd ./  #没有第二个参数,默认当前目录,其实也可以不用写,但我就想写上去..
+    fi
+    echo "$a"
+    if [ ${file1##*.} = "gz" ]  #下面就是根据不同的后缀来使用不同的命令
     then
         tar -zxvf $1
-    elif [ i = bz2 ]
+    elif [ "${file1##*.}" = "bz2" ]
     then
         tar -jxvf $1
-    elif [ i = tar ]
+    elif [ "${file1##*.}" = "tar" ]
     then
         tar -xvf $1
     else
         unzip $1
     fi
-else
+else  #当参数不满足上述两种条件时,显示usage
     echo "usage: $0 [--list] or [Source compressed file] [Destination path]"
     echo "Self compression.sh according to the file name suffix"
 fi
