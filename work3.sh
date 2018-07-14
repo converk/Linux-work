4ef605270e0293db2cc35c560514757cbf7844a8
ping <843951249@qq.com>
2018-07-14 15:55:26 +0800

    work3

--- work3.sh
+++ work3.sh
@@ -1,4 +1,4 @@
-if [ $# -ne 0 -a $# -ne 4 ]  #当传进来的参数不全时,输出提示
+if [ $# -ne 0 -a $# -ne 4 -a $# -ne 2 ]  #当传进来的参数不全时,输出提示
 then   
     echo "usage: $0 [-n N] [ -d DIR ]"
     echo "Show top N largest files/directories"
@@ -9,13 +9,23 @@ else
         then
             echo "usage: $0 [-n N] [ -d DIR ]"
             echo "Show top N largest files/directories"
+        else
+            du -sh $4* | sort -rn | head -$2
         fi
     fi
     if [ $# -eq 0 ]  #当没有参数的时候
     then
          du -sh * | sort -rn  #打印出当前目录下的文件排序
-    else
-        du -sh $4* | sort -rn | head -$2
     fi
-
+    if [ $# -eq 2 ]   #当有两个参数时分情况讨论
+    then
+        if [ $1 = "-n" ]
+        then
+            du -sh * | sort -rn | head -$2
+        fi
+        if [ $1 = "-d" ]
+        then
+            du -sh $2* | sort -rn
+        fi
+    fi
 fi
